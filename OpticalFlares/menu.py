951c38831b5f078175e9714fafbeecee4732c4bb
pascalandr <pascalandr@gmail.com>
2021-09-08 16:02:52 +0200

    initial comit

---
+++ OpticalFlares/menu.py
@@ -0,0 +1,12 @@
+
+
+import sys
+import os.path
+import nuke
+
+toolbar = nuke.toolbar("Nodes")
+toolbar.addMenu("VideoCopilot", icon="VideoCopilot.png")
+toolbar.addCommand( "VideoCopilot/OpticalFlares", "nuke.createNode('OpticalFlares')", icon="OpticalFlares.png")
+
+
+
---
+++ init.py
@@ -0,0 +1,8 @@
+nuke.pluginAddPath( './Gizmos' )
+nuke.pluginAddPath( './OpticalFlares' )
+nuke.pluginAddPath( './Cryptomatte' )
+nuke.pluginAddPath('./higx/PointRender')
+
+import os
+
+os.environ['OFX_PLUGIN_PATH'] = 'X:/Workgroup_Nuke/OFX/Plugins'
